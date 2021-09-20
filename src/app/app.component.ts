a406f17ec27efb7ccc342ac307dedb8f87bf01a0
FazioNico <contact@nicolasfazio.ch>
2021-09-20 12:41:53 +0200

    feat: pwa utils

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AdditiveFinder';
+
+  constructor(
+    private _toastCtrl: ToastController
+  ) {
+    this.displayToastInstall();
+  }
+
+  async displayToastInstall(platform = null) {
+    // Detects if device is on iOS 
+    const isIos = () => {
+      const userAgent = platform || window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test( userAgent );
+    }
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => ('standalone' in (window as any).navigator) && ((window as any).navigator.standalone);
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+      const toast = await this._toastCtrl.create({
+        message: 'AdditiveFinder is not installed. Please install it by click to ....',
+      });
+      await toast.present();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { UpdatesNotificationComponent } from './notif-component/notif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdatesNotificationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     IonicModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
