510ef6b19fcbb96a8a127dc3161bb9f759347785
FazioNico <contact@nicolasfazio.ch>
2021-09-29 13:07:49 +0200

    feat: use methode with correct data

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { IAdditive } from 'src/app/additive.iinterface';
 import { AdditiveService } from 'src/app/services/additive.service';
+import { collection, Firestore } from '@angular/fire/firestore';
+import { collectionData } from 'rxfire/firestore';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
   public max: number = 15;
 
   public additives: IAdditive[]|undefined;
   constructor(
-    private readonly _api: AdditiveService
+    private readonly _api: AdditiveService,
+    private readonly _firestore: Firestore
+
   ) { }
 
-  async ngOnInit() {
+  async ionViewWillEnter() {
     const additives = await this._api.getAll();
-    const views: any[] = [];
-    this.additives = this.aggregateData(additives, views);
+    const fbCol = collection(this._firestore, 'additives');
+    const data: any[] = await collectionData(fbCol).pipe(first()).toPromise();
+    this.additives = this.aggregateData(additives, data);
   }
 
   aggregateData(a: IAdditive[], b: {additiveId: string, views: number}[]) {
