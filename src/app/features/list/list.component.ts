21f486f929876ad2725599f42e60c7d635cb4746
FazioNico <contact@nicolasfazio.ch>
2021-09-30 10:40:41 +0200

    feat: add filter

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,6 +4,9 @@ import { AdditiveService } from 'src/app/services/additive.service';
 import { collection, Firestore } from '@angular/fire/firestore';
 import { collectionData } from 'rxfire/firestore';
 import { first } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
+
+type SortType = 'alphabetic'|'views';
 
 @Component({
   selector: 'app-list',
@@ -13,11 +16,13 @@ import { first } from 'rxjs/operators';
 export class ListComponent {
 
   public max: number = 15;
+  public sortByValue: SortType = 'alphabetic';
 
   public additives: IAdditive[]|undefined;
   constructor(
     private readonly _api: AdditiveService,
-    private readonly _firestore: Firestore
+    private readonly _firestore: Firestore,
+    private readonly _alertCtrl: AlertController
 
   ) { }
 
@@ -46,5 +51,67 @@ export class ListComponent {
       this.max += 15;
     }
     $event.target.complete();
+  }
+
+  async selectSortBy() {
+    const ionAlert = await this._alertCtrl.create({
+      header: 'Sortby',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Alphabetic',
+          value: 'alphabetic',
+          checked: this.sortByValue === 'alphabetic' ? true : false,
+          cssClass: 'alertRadio'
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Views',
+          value: 'views',
+          checked: this.sortByValue === 'views' ? true : false,
+          cssClass: 'alertRadio'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'ion-color-dark'
+        }, 
+        {
+          text: 'Ok', 
+          role: 'ok',
+          cssClass: 'ion-color-dark'
+        }
+      ]
+    });
+    await ionAlert.present();
+    const { data, role } = await ionAlert.onWillDismiss();
+    console.log(data, role);
+    if (role === 'ok' && data?.values) {
+      this.sortBy(data?.values)
+    }
+  }
+
+  sortBy(type: SortType){
+    
+    if (type === 'alphabetic' && this.additives) {  
+      this.additives = this.additives.sort((a, b) => {
+        return (+a.id) - (+b.id)
+      });
+    }
+
+    if (type === 'views' && this.additives) {
+      this.additives = this.additives.sort((a, b) => {
+        return (b.views||0) - (a.views||0) 
+      });
+    }
+    this.sortByValue = type;
+  }
+
+  trackBy(index: number, additive: IAdditive): string {
+    return additive.id;
   }
 }
